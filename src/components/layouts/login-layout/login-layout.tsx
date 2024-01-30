41486a219b639cd6b7e0992a45a3d8cb28a8383c
Beto Freitas <gilbertofreitas997@gmail.com>
2024-01-30 14:43:48 -0300

    feat: added lazy loading fallback

--- src/components/layouts/login-layout/login-layout.tsx
+++ src/components/layouts/login-layout/login-layout.tsx
@@ -1,9 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Loader2 } from 'lucide-react';
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
 
 export const LoginLayout = () => (
   <div className='dark text-zinc-100 flex flex-col items-center justify-center h-dvh w-screen bg-zinc-950 overflow-hidden'>
     <div className='max-w-sm space-y-6'>
-      <Outlet />
+      <Suspense
+        fallback={
+          <div className='animate-spin'>
+            <Loader2 className='size-8' />
+          </div>
+        }>
+        <Outlet />
+      </Suspense>
     </div>
   </div>
 );
